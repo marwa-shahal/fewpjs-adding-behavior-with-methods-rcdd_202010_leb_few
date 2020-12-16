9ed61390a3c2d21b97867780422a46514d9c97ad
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-16 12:17:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ class Cat {
     this.sex=sex;
   }
   speak (){
+<<<<<<< HEAD
     return this.name + " says meow!";
   }
  }
@@ -18,6 +19,21 @@ class Cat {
     return this.name+" says woof!";
     }
  }
+=======
+    return console.log(`${this.name}says meow!`);
+  }
+}
+ 
+class Dog {
+  constructor(name,sex){
+    this.name=name;
+    this.sex=sex;
+  }
+  speak (){
+   return console.log(`${this.name}says woof!`);
+  }
+}
+>>>>>>> 2c008e11bb015787f9d3aa86c02235bca6b4ee03
  
 class Bird {
   constructor(name,sex){
@@ -25,11 +41,19 @@ class Bird {
     this.sex=sex;
   }
   speak (){
+<<<<<<< HEAD
     if(this.sex === "male"){
    return `It's me! ${this.name}, the parrot!`;
      }
     else {
       return `${this.name} says squawk!`;
+=======
+    if(this.sex===male){
+   return console.log(`It's me! ${this.name}`);
+     }
+    else {
+      return console.log(`${this.name}says squawk!`);
+>>>>>>> 2c008e11bb015787f9d3aa86c02235bca6b4ee03
   }
   }
 }
